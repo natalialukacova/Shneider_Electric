c25227d4adb14ac9d931d856945c6380ea2681f4
salmacx <145067369+salmacx@users.noreply.github.com>
2024-04-30 13:05:37 +0200

    initial gui

---
+++ src/gui/controller/MainViewController.java
@@ -0,0 +1,54 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainViewController {
+
+    @FXML
+    private Button addCountryBtn;
+
+    @FXML
+    private Button addTeamBtn;
+
+    @FXML
+    private Button closeWindow;
+
+    @FXML
+    void addCountryPopUp(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/addCountry.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void addTeamPopUp(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/view/addTeam.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        currentStage.close();
+    }
+}
